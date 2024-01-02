95951418700745d097a8281ef3e553ae70b660c6
lucas.ms(이문성)/kakao <lucas.ms@kakaocorp.com>
2024-01-02 17:00:38 +0900

    add random picture background

---
+++ components/RandomBackground.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import styles from "./RandomBackground.module.css";
+
+function RandomBackground() {
+  const id = Math.floor(Math.random() * 1000);
+  const width = 1920;
+  const height = 1080;
+  const seed = `https://picsum.photos/seed/${id}/${width}/${height}`;
+
+  return (
+    <div className={styles.background}>
+      <Image
+        src={seed}
+        alt="random background"
+        layout="fill"
+        objectFit="cover"
+        objectPosition="center"
+      />
+    </div>
+  );
+}
+
+export default RandomBackground;
